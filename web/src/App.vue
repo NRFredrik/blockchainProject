27ceddc71ac154f74963f46a6fa07626ca7f4d01
potatoshoe <jjmau14@gmail.com>
2019-04-21 13:55:20 -0600

    Initial web stuff - just a basic dashboard layout with a single web3 api call to load the current eth for my address - more to come soon

---
+++ web/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,sans-serif;
+    color: #3d5170
+  }
+</style>
